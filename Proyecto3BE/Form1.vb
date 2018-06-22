42ed2fd5de2b0e835419c3eced87bb787a5cd6dc
Zyro <38949958+0Zyro@users.noreply.github.com>
2018-06-22 14:01:42 -0300

    Login (En Proceso)

---
+++ Proyecto3BE/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        If Label1.Text = "usuario" And Label2.Text = "contraseña" Then
+
+            Form2.Show()
+
+        Else
+
+            Label3.Text = "Datos Incorrectos"
+
+        End If
+
+    End Sub
+End Class
