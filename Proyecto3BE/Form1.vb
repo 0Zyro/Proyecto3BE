a3934310b911f9ee23d47fe39a8e202f130de656
Zyro <38949958+0Zyro@users.noreply.github.com>
2018-06-25 10:48:13 -0300

    Login
    Comentarios

--- Proyecto3BE/Form1.vb
+++ Proyecto3BE/Form1.vb
@@ -1,35 +1,39 @@
 ﻿Public Class Form1
 
+    'Cuando el usuario presiona "Aceptar" se evaluaran los datos y se hara login en caso de ser correctos
+    'en caso de no serlo, se informara el error
     Private Sub ButtonAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonAceptar.Click
-
         If TextBoxUser.Text = "user" And TextBoxPasswd.Text = "passwd" Then
             Form2.Show()
             Me.Close()
         Else
-            TextBoxUser.Text = ""
             TextBoxPasswd.Text = ""
             LabelInfo.Text = "Datos Incorrectos"
             TextBoxUser.Focus()
         End If
     End Sub
 
+    'Si usuario presiona "Enter" en el TextBox "Usuario" se cambia el focus al TextBox "Passwd" para continuar escribiendo alli
     Private Sub TextBoxUser_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles TextBoxUser.KeyDown
         If e.KeyCode = Keys.Enter Then
             TextBoxPasswd.Focus()
         End If
     End Sub
 
-    Private Sub TextBoxUser_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBoxUser.TextChanged
-        LabelInfo.Text = ""
-    End Sub
-
+    'Si usuario presiona "Enter" en el TextBox "Contraseña" se evaluan los datos para hacer el login
     Private Sub TextBoxPasswd_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles TextBoxPasswd.KeyDown
         If e.KeyCode = Keys.Enter Then
             ButtonAceptar.PerformClick()
         End If
     End Sub
 
+    'Si usuario cambia el texto escrito en el TextBox "Contraseña" se deja de indicar que los datos son erroneos
     Private Sub TextBoxPasswd_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBoxPasswd.TextChanged
+        LabelInfo.Text = ""
+    End Sub
+
+    'Si usuario cambia el texto escrito en el TextBox "Usuario" se deja de indicar que los datos son erroneos
+    Private Sub TextBoxUser_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles TextBoxUser.TextChanged
         LabelInfo.Text = ""
     End Sub
 End Class
