a43b9493e4f7dedd6d0ae2a196b505e60396adae
Zyro <38949958+0Zyro@users.noreply.github.com>
2018-06-27 17:16:19 -0300

    Conex Database

--- Proyecto3BE/Form2.vb
+++ Proyecto3BE/Form2.vb
@@ -1,3 +1,20 @@
-﻿Public Class Form2
+﻿Imports MySql.Data.MySqlClient
 
+Public Class Form2
+
+    'Dim data As String = ("Server=localhost;Database=dbproyecto;User id=root;Password=;Port=3306;")
+    Dim data As String = ("Server=www.db4free.net;Database=database_vacas;User id=zero22394;Password=zero22394;Port=3306;")
+
+    Dim conexion As New MySqlConnection(data)
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Try
+            conexion.Open()
+            MsgBox("Conectado")
+        Catch ex As Exception
+            MsgBox("Error:" And ex.Message)
+        End Try
+
+    End Sub
 End Class
