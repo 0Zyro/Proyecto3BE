20ca97045d0e3a1c664f6bce1f2e17757feb7e6e
DiegoProyecto <39371942+DiegoProyecto@users.noreply.github.com>
2018-06-27 21:36:16 -0300

    Modifique el conector
    Tu coneccion lo deje en comentario proba ese que puse.Yo  me acostumbre a usar de ese modo Fijate si anda asi

--- Proyecto3BE/Form2.Designer.vb
+++ Proyecto3BE/Form2.Designer.vb
@@ -25,11 +25,27 @@ Partial Class Form2
         Me.PanelUsuario = New System.Windows.Forms.Panel()
         Me.TabbedPane = New System.Windows.Forms.TabControl()
         Me.TabGanado = New System.Windows.Forms.TabPage()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.TabVentas = New System.Windows.Forms.TabPage()
         Me.TabCompras = New System.Windows.Forms.TabPage()
         Me.TabUsuarios = New System.Windows.Forms.TabPage()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
         Me.TabbedPane.SuspendLayout()
+        Me.TabGanado.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelUsuario
@@ -54,6 +70,21 @@ Partial Class Form2
         '
         'TabGanado
         '
+        Me.TabGanado.Controls.Add(Me.Button3)
+        Me.TabGanado.Controls.Add(Me.Button2)
+        Me.TabGanado.Controls.Add(Me.Button1)
+        Me.TabGanado.Controls.Add(Me.TextBox5)
+        Me.TabGanado.Controls.Add(Me.TextBox4)
+        Me.TabGanado.Controls.Add(Me.Label5)
+        Me.TabGanado.Controls.Add(Me.Label4)
+        Me.TabGanado.Controls.Add(Me.Label3)
+        Me.TabGanado.Controls.Add(Me.TextBox3)
+        Me.TabGanado.Controls.Add(Me.TextBox2)
+        Me.TabGanado.Controls.Add(Me.Label2)
+        Me.TabGanado.Controls.Add(Me.TextBox1)
+        Me.TabGanado.Controls.Add(Me.Label1)
+        Me.TabGanado.Controls.Add(Me.DataGridView1)
+        Me.TabGanado.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TabGanado.Location = New System.Drawing.Point(4, 22)
         Me.TabGanado.Name = "TabGanado"
         Me.TabGanado.Padding = New System.Windows.Forms.Padding(3)
@@ -61,6 +92,121 @@ Partial Class Form2
         Me.TabGanado.TabIndex = 0
         Me.TabGanado.Text = "Ganado"
         Me.TabGanado.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(448, 323)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "Eliminar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(448, 268)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Modificar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(446, 215)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Agregar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(516, 162)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(156, 20)
+        Me.TextBox5.TabIndex = 10
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(516, 129)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(47, 20)
+        Me.TextBox4.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(438, 162)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(46, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Estado"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(438, 129)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Peso inicial"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(437, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Edad"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(516, 97)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(47, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(516, 65)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(62, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(437, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Sexo"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(516, 32)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(436, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Raza"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 6)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(412, 428)
+        Me.DataGridView1.TabIndex = 0
         '
         'TabVentas
         '
@@ -106,7 +252,7 @@ Partial Class Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(784, 462)
+        Me.ClientSize = New System.Drawing.Size(784, 461)
         Me.Controls.Add(Me.TabbedPane)
         Me.Controls.Add(Me.PanelUsuario)
         Me.MaximumSize = New System.Drawing.Size(800, 500)
@@ -115,6 +261,9 @@ Partial Class Form2
         Me.ShowIcon = False
         Me.Text = "Form2"
         Me.TabbedPane.ResumeLayout(False)
+        Me.TabGanado.ResumeLayout(False)
+        Me.TabGanado.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -125,4 +274,18 @@ Partial Class Form2
     Friend WithEvents TabCompras As System.Windows.Forms.TabPage
     Friend WithEvents TabUsuarios As System.Windows.Forms.TabPage
     Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
 End Class
--- Proyecto3BE/Form2.vb
+++ Proyecto3BE/Form2.vb
@@ -1,20 +1,52 @@
-﻿Imports MySql.Data.MySqlClient
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports MySql.Data.MySqlClient
 
 Public Class Form2
 
     'Dim data As String = ("Server=localhost;Database=dbproyecto;User id=root;Password=;Port=3306;")
     Dim data As String = ("Server=www.db4free.net;Database=database_vacas;User id=zero22394;Password=zero22394;Port=3306;")
 
-    Dim conexion As New MySqlConnection(data)
+    Public Conexion As MySqlDataAdapter
+    Public Tabla As DataTable
+    Public Consulta As String
+    Public MysqlConexion As MySqlConnection = New MySqlConnection(data)
+
+    Public Sub consultar()
+
+        Try
+
+            Conexion = New MySqlDataAdapter(Consulta, data)
+            Tabla = New DataTable
+            Conexion.Fill(Tabla)
+
+
+        Catch ex As Exception
+
+            MsgBox(ex.Message)
+
+        End Try
+
+
+    End Sub
+
+    'Dim conexion As New MySqlConnection(data)
 
     Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-        Try
-            conexion.Open()
-            MsgBox("Conectado")
-        Catch ex As Exception
-            MsgBox("Error:" And ex.Message)
-        End Try
+        'Try
+        '    conexion.Open()
+        '    MsgBox("Conectado")
+        'Catch ex As Exception
+        '    MsgBox("Error:" And ex.Message)
+        'End Try
 
     End Sub
+
+    Private Sub TabGanado_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TabGanado.Click
+
+    End Sub
+
+    
+   
 End Class
