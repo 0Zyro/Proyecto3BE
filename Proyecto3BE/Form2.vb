54858328ff379dfe87d737c4b62e017c49b7d451
Zyro <38949958+0Zyro@users.noreply.github.com>
2018-06-30 11:43:58 -0300

    Usuarios v1.1

--- Proyecto3BE/Form2.vb
+++ Proyecto3BE/Form2.vb
@@ -173,47 +173,42 @@ Public Class Form2
     Dim rows(0) As String
     Dim tipo As String
 
-    Private Sub BotonBusquedaUsuarios_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BotonBusquedaUsuarios.Click
+    Dim connection As New MySqlConnection(data)
+    Dim comando As New MySqlCommand
+    Dim reader As MySqlDataReader
 
-        Dim conexion As New MySqlConnection(data)
-        Dim comando As New MySqlCommand
-        Dim reader As MySqlDataReader
+    Private Sub BotonBusquedaUsuarios_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BotonBusquedaUsuarios.Click
 
         If TextBoxBusquedaUsuarios.Text = "" Then
             comando.CommandText = "select ci from usuarios"
         Else
             comando.CommandText = ("select nombre from usuario where " + campos(ComboBoxUsuarios.SelectedIndex) + "='" + TextBoxBusquedaUsuarios.Text + "'")
-            Select Case ComboBoxUsuarios.SelectedIndex
-                Case 0
-                    tipo = "Integer"
-                Case 1
-                    tipo = "String"
-                Case 2
-                    tipo = "String"
-            End Select
         End If
         comando.CommandType = CommandType.Text
-        comando.Connection = conexion
+        comando.Connection = connection
 
         Try
-            conexion.Open()
+            connection.Open()
             reader = comando.ExecuteReader()
             If reader.HasRows Then
                 ListBoxUsuarios.Items.Clear()
+                ReDim rows(0)
+
                 While (reader.Read())
-                    If tipo = "Integer" Then
-                        rows(rows.Length - 1) = reader.GetInt32(0).ToString
-                    Else
-                        rows(rows.Length - 1) = reader.GetString(0)
-                    End If
+                    rows(rows.Length - 1) = reader.GetString("nombre")
                     ReDim Preserve rows(rows.Length)
                 End While
-                ReDim Preserve rows(rows.Length - 2)
-                ListBoxUsuarios.Items.AddRange(rows)
+                'ReDim Preserve rows(rows.Length - 2)
+
+                For Each ele As String In rows
+                    MsgBox(ele + ".")
+                Next
+
+                'ListBoxUsuarios.Items.AddRange(rows)
             Else
                 LabelInfoUsuarios.Text = "No se encontraron resultados"
             End If
-            conexion.Close()
+            connection.Close()
         Catch ex As Exception
 
         End Try
