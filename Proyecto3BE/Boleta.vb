e1bd9c6bc505e912fc15429e41b04e67f6fde235
Zyro <timo.22394@hotmail.com>
2019-04-22 19:42:21 -0300

    Revert "se borra todo"
    This reverts commit d0acbb717f845728ed0db1dca7b805000431508b.

---
+++ Proyecto3BE/Boleta.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports MySql.Data.MySqlClient
+Public Class Boleta
+    Public Conexion As MySqlDataAdapter
+    Public Tabla As DataTable
+    Public Consulta As String
+    Dim data As String = ("Server=localhost;Database=vacas;User id=root;Password=;Port=3306;")
+    Dim connection As New MySqlConnection(data)
+    Dim comando As New MySqlCommand
+    Dim reader As MySqlDataReader
+
+
+    Public Sub clear()
+        DGVBOLETA.Rows.Clear()
+    End Sub
+
+
+
+    Public Sub cargardatos(ByVal peso As Double, ByVal precio As Double, ByVal subtotal As Double, ByVal idganado As Integer, ByVal fechaventa As String, ByVal total As Double, ByVal nombreclientev As String, ByVal apellidoclientev As String, ByVal idventaboleta As Integer, ByVal raza As String, ByVal sexo As String)
+        LBLNombreclientev.Text = nombreclientev
+        lblapellidoenboleta.Text = apellidoclientev
+        txbfechaboleta.Text = fechaventa
+        lblidventabo.Text = idventaboleta
+        DGVBOLETA.Rows.Add()
+        DGVBOLETA.Item(0, DGVBOLETA.Rows.Count - 1).Value = idganado
+        DGVBOLETA.Item(1, DGVBOLETA.Rows.Count - 1).Value = raza
+        DGVBOLETA.Item(2, DGVBOLETA.Rows.Count - 1).Value = sexo
+        DGVBOLETA.Item(3, DGVBOLETA.Rows.Count - 1).Value = peso
+        DGVBOLETA.Item(4, DGVBOLETA.Rows.Count - 1).Value = precio
+        DGVBOLETA.Item(5, DGVBOLETA.Rows.Count - 1).Value = subtotal
+        TXTSubtotal.Text = (total * 100) / 122
+        TXTIVA.Text = ((total * 22) / 122).ToString
+        TXTTotalGeneral.Text = total.ToString
+    End Sub
+    Private Sub imprimir()
+        Try
+            Cursor.Current = Cursors.WaitCursor 'flecha  redonda
+            With Me.PrintForm1
+                .PrintAction = Printing.PrintAction.PrintToPreview ' evento q hace que imprima el form
+                .PrinterSettings.DefaultPageSettings.Landscape = True 'vista preeliminar
+                .Print(Me, PowerPacks.Printing.PrintForm.PrintOption.ClientAreaOnly)
+            End With
+            Cursor.Current = Cursors.Default ' cambia el cursor al normal
+        Catch ex As Exception
+            Cursor.Current = Cursors.Default
+            MsgBox(ex.ToString, MsgBoxStyle.Critical, "error al tratar de imprimir")
+        End Try
+    End Sub
+    Private Sub BTNimprimir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BTNimprimir.Click
+        BTNimprimir.Hide()
+        imprimir()
+        BTNimprimir.Show()
+    End Sub
+
+
+
+    Private Sub Boleta_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ Proyecto3BE/Login.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list. hola
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelUser = New System.Windows.Forms.Label()
+        Me.TextBoxUser = New System.Windows.Forms.TextBox()
+        Me.LabelInfo = New System.Windows.Forms.Label()
+        Me.BotonAceptar = New System.Windows.Forms.Button()
+        Me.BotonVolver = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LabelUser
+        '
+        Me.LabelUser.AutoSize = True
+        Me.LabelUser.Location = New System.Drawing.Point(13, 13)
+        Me.LabelUser.Name = "LabelUser"
+        Me.LabelUser.Size = New System.Drawing.Size(65, 13)
+        Me.LabelUser.TabIndex = 0
+        Me.LabelUser.Text = "Usuario (CI):"
+        '
+        'TextBoxUser
+        '
+        Me.TextBoxUser.Location = New System.Drawing.Point(12, 38)
+        Me.TextBoxUser.Name = "TextBoxUser"
+        Me.TextBoxUser.Size = New System.Drawing.Size(244, 20)
+        Me.TextBoxUser.TabIndex = 1
+        '
+        'LabelInfo
+        '
+        Me.LabelInfo.AutoSize = True
+        Me.LabelInfo.Location = New System.Drawing.Point(13, 61)
+        Me.LabelInfo.Name = "LabelInfo"
+        Me.LabelInfo.Size = New System.Drawing.Size(0, 13)
+        Me.LabelInfo.TabIndex = 4
+        '
+        'BotonAceptar
+        '
+        Me.BotonAceptar.Location = New System.Drawing.Point(181, 86)
+        Me.BotonAceptar.Name = "BotonAceptar"
+        Me.BotonAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.BotonAceptar.TabIndex = 5
+        Me.BotonAceptar.Text = ">>"
+        Me.BotonAceptar.UseVisualStyleBackColor = True
+        '
+        'BotonVolver
+        '
+        Me.BotonVolver.Location = New System.Drawing.Point(12, 86)
+        Me.BotonVolver.Name = "BotonVolver"
+        Me.BotonVolver.Size = New System.Drawing.Size(75, 23)
+        Me.BotonVolver.TabIndex = 6
+        Me.BotonVolver.Text = "<<"
+        Me.BotonVolver.UseVisualStyleBackColor = True
+        Me.BotonVolver.Visible = False
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(268, 111)
+        Me.Controls.Add(Me.BotonVolver)
+        Me.Controls.Add(Me.BotonAceptar)
+        Me.Controls.Add(Me.LabelInfo)
+        Me.Controls.Add(Me.TextBoxUser)
+        Me.Controls.Add(Me.LabelUser)
+        Me.MaximumSize = New System.Drawing.Size(284, 150)
+        Me.MinimumSize = New System.Drawing.Size(284, 150)
+        Me.Name = "Login"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ingresar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LabelUser As System.Windows.Forms.Label
+    Friend WithEvents TextBoxUser As System.Windows.Forms.TextBox
+    Friend WithEvents LabelInfo As System.Windows.Forms.Label
+    Friend WithEvents BotonAceptar As System.Windows.Forms.Button
+    Friend WithEvents BotonVolver As System.Windows.Forms.Button
+
+End Class
